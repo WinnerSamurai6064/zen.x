1d24a85eb66d6e19ed8342b7ff630386175da99d
WinnerSamurai6064 <ikegbuneagwu@gmail.com>
2026-06-12 16:39:42 +0100

    Add phase one app shell

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ClothScene from "./components/ClothScene.jsx";
+import Win95Loader from "./components/Win95Loader.jsx";
+
+export default function App() {
+  return (
+    <main className="app-shell">
+      <ClothScene />
+      <Win95Loader />
+    </main>
+  );
+}
